2a5fb62ad485f3d9b5f8f7c638df407b98ae8bc9
Yago Luiz <yago.luiz@hotmail.com>
2025-07-13 19:07:33 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rinha-de-backend-2025
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/rinha_de_backend_2025/core.clj
@@ -0,0 +1,7 @@
+(ns rinha-de-backend-2025.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
