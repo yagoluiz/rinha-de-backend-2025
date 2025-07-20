d11b29fd29982fdf8cddeb097b558350ff377567
Yago Luiz <yago.luiz@hotmail.com>
2025-07-20 16:47:06 -0300

    Fix requested_at column timezone

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE payments (
   correlation_id UUID UNIQUE NOT NULL,
   amount NUMERIC(10,2) NOT NULL,
   processor processor_type NOT NULL,
-  requested_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+  requested_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE INDEX idx_payments_processor ON payments(processor);
