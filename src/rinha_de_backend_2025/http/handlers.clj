dd9f3544281ec002fbaadbfb7500cc4f3f1266ef
Yago Luiz <yago.luiz@hotmail.com>
2025-07-20 16:16:21 -0300

    Init payment summary route

--- src/rinha_de_backend_2025/http/handlers.clj
+++ src/rinha_de_backend_2025/http/handlers.clj
@@ -1,7 +1,15 @@
 (ns rinha-de-backend-2025.http.handlers
-  (:require [rinha-de-backend-2025.workers.processor :as processor]))
+  (:require [rinha-de-backend-2025.workers.processor :as processor])
+  (:import (java.time Instant)))
 
-(defn payment-request! [req]
-  (let [payment (:body req)]
+(defn payment-request! [request]
+  (let [payment (:body request)]
     (processor/producer! payment)
     {:status 202}))
+
+(defn payment-summary! [request]
+  (let [params (:params request)
+        from   (Instant/parse (:from params))
+        to     (Instant/parse (:to params))
+        _      (println "Payment summary request from" from "to" to)]
+    {:status 200}))
--- src/rinha_de_backend_2025/http/router.clj
+++ src/rinha_de_backend_2025/http/router.clj
@@ -1,16 +1,24 @@
 (ns rinha-de-backend-2025.http.router
   (:require [reitit.ring :as ring]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
             [rinha-de-backend-2025.http.handlers :as handlers]))
 
-(def ^:private middlewares
-  [[wrap-json-body {:keywords? true}]
-   wrap-json-response])
+(def ^:private common-middlewares [[wrap-json-response]])
 
 (def ^:private routes
-  [["/payments" {:post handlers/payment-request!}]])
+  [["/payments"
+    {:post       handlers/payment-request!
+     :middleware (conj common-middlewares
+                       [wrap-json-body {:keywords? true}])}]
+   ["/payments-summary"
+    {:get        handlers/payment-summary!
+     :middleware (conj common-middlewares
+                       [wrap-params]
+                       [wrap-keyword-params])}]])
 
 (defn app-handler []
   (ring/ring-handler
-    (ring/router routes {:data {:middleware middlewares}})
+    (ring/router routes)
     ring/create-default-handler))
